14c3c389fafb93974f649ff5bec734d85b2040cf
igorFerreiraB <igorfgb2006@gmail.com>
2025-12-20 11:46:53 -0300

    feat: vinculo de userId para placa consultada

--- app/Models/PlacaCache.php
+++ app/Models/PlacaCache.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class PlacaCache extends Model
 {
-    protected $fillable = ['placa', 'dados'];
+    protected $fillable = ['placa', 'dados', 'user_id'];
     protected $casts = [
         'dados' => 'json'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
